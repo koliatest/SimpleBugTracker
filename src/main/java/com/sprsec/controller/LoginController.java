12db3a4aa44d424187c639d723f16ad2ac47e978
koliatest <asicslol1@mail.ru>
2016-04-24 14:20:05 +0300

    Changed security roles strategy, added landing page

---
+++ src/main/java/com/sprsec/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.sprsec.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView login(
+            @RequestParam(value = "error", required = false) String error) {
+
+        ModelAndView model = new ModelAndView("/login-form");
+        if (error != null) {
+            model.addObject("error", "ERROR");
+        }
+
+        return model;
+
+    }
+
+}
